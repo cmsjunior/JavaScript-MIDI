5fd34d77655d32bfb4bea5d8ea05d3c3a8380699
Cleber Junior <csjuniorgtr@gmail.com>
2024-12-21 18:44:43 -0300

    atualizando versao

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoSom){
+    document.querySelector(idElementoSom).play();
 }
 
-document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+    
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`;
+    //console.log(idAudio);
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+    contador = contador + 1;
+
+    //console.log(contador);
+}
